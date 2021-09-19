00c229e4e1d310f9e12577d30514a738a79f4dc8
Maciej Wikira <maciej.daniel.wikira@gmail.com>
2021-09-19 19:32:43 +0200

    feat(stats): add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     post '/trips', to: 'trips#create'
+
+    namespace :stats do
+      get '/weekly', to: 'stats#weekly'
+      get '/monthly', to: 'stats#monthly'
+    end
   end
 end
