3f3f953a5b41d98bb119dae8c282958300e7e185
Maciej Wikira <maciej.daniel.wikira@gmail.com>
2021-09-19 21:44:27 +0200

    fix(stats): handle weekly stats when no trips

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,14 +5,17 @@ class Trip < ApplicationRecord
 
     def self.weekly_summary
         stats = Trip.where("delivery_date >= ?", current_week).pluck("SUM(price), SUM(distance)")
+        total_price = stats[0][0].nil? ? 0 : stats[0][0]
+        total_distance_meters = stats[0][1].nil? ? 0 : stats[0][1]
         return {
-            :total_distance => "#{stats[0][1]/1000}km",
-            :total_price => "#{stats[0][0].round(2)}PLN"
+            :total_distance => "#{total_distance_meters/1000}km",
+            :total_price => "#{total_price.round(2)}PLN"
         }
     end
 
     def self.monthly_summary
         stats = Trip.where("delivery_date >= ?", current_month).group("delivery_date").pluck("delivery_date, SUM(distance), AVG(distance), AVG(price)")
+
         stats.map{|s| {
             :day => "#{Date::MONTHNAMES[s[0].mon]}, #{s[0].mday.ordinalize}",
             :total_distance => "#{s[1]/1000}km",
