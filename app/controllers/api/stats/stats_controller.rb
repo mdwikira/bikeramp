c75edf157c56efeb0e5fa1348d7cc3b21c1bada5
Maciej Wikira <maciej.daniel.wikira@gmail.com>
2021-09-19 19:35:35 +0200

    feat(stats): add controllers

---
+++ app/controllers/api/stats/stats_controller.rb
@@ -0,0 +1,9 @@
+class Api::Stats::StatsController < ApplicationController
+    def weekly
+        render json: {"stub": "response"}, status: :ok
+    end
+
+    def monthly
+        render json: {"stub": "response"}, status: :ok
+    end
+end
